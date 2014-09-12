ea511712cbedf31a144b5382f44ab9bf527cd2fb
Tsung Hung <hungmasterj@gmail.com>
2014-09-12 10:53:51 -0700

    added apple itunes to get back movies and beginnging the process of parsing

--- TopMovies/ViewController.swift
+++ TopMovies/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    let viewModel = ViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        viewModel.fetchItems {
+            dispatch_async(dispatch_get_main_queue()) {
+                self.tableView.reloadData()
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return viewModel.numberOfSections()
+    }
 
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.numberOfItemsInSection(section)
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as UITableViewCell
+        configureCell(cell, atIndexPath: indexPath)
+        return cell
+    }
+    
+    func configureCell(cell: UITableViewCell, atIndexPath indexPath:NSIndexPath) {
+        cell.textLabel?.text = viewModel.titleForItemAtIndexPath(indexPath)
+    }
 
 }
 
--- TopMovies/ViewModel.swift
+++ TopMovies/ViewModel.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+class ViewModel {
+    
+    let urlString = "http://itunes.apple.com/rss/topmovies/limit=25/json"
+    var titles = [String()]
+    
+    func fetchItems(success: () -> ()) {
+        let url = NSURL(string: urlString)
+        let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
+        let task = session.dataTaskWithURL(url) { (data, response, error) in var jsonError: NSError?
+            let json = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &jsonError) as NSDictionary
+            
+            if let unwrappedError = jsonError { //not the best loggin system
+                println("json error: \(unwrappedError)")
+            } else {
+                self.titles = json.valueForKeyPath("feed.entry.im:name.label") as [String] //not the most optimized version
+            }
+        }
+    }
+    
+    func numberOfSections() -> Int {
+        return 1
+    }
+    
+    func numberOfItemsInSection(section: Int) -> Int {
+        return titles.count
+    }
+    
+    func titleForItemAtIndexPath(indexPath: NSIndexPath) -> String {
+        return titles[indexPath.row]
+    }
+    
+}
